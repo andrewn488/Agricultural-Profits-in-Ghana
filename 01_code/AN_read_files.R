37eb8f79a93fda4339c6437e57321b344da3c4b8
Andrew Nalundasan <analundasan@seattleu.edu>
2020-11-16 18:22:48 -0800

    committing read_files code for andrew branch.

---
+++ 01_code/AN_read_files.R
@@ -0,0 +1,79 @@
+# data translation project
+# author: Andrew Nalundasan
+# team: Arunima Roy, Brian Hsu
+# for: OMSBA 5112, Seattle University
+# date: 11/20/2020
+
+
+# load libraries
+library(tidyverse)
+library(haven)
+
+# read all dta files:
+sec0a <- read_dta("02_raw_data/sec0a.dta")
+View(sec0a)
+summary(sec0a)
+
+# sec8a1
+sec8a1 <- read_dta("02_raw_data/sec8a1.dta")
+View(sec8a1)
+summary(sec8a1)
+
+# sec8a2
+sec8a2 <- read_dta("02_raw_data/sec8a2.dta")
+View(sec8a2)
+summary(sec8a2)
+
+# sec8a3
+sec8a3 <- read_dta("02_raw_data/sec8a3.dta")
+View(sec8a3)
+summary(sec8a3)
+
+# sec8b
+sec8b <- read_dta("02_raw_data/sec8b.dta")
+View(sec8b)
+summary(sec8b)
+
+# sec8c1
+sec8c1 <- read_dta("02_raw_data/sec8c1.dta")
+View(sec8c1)
+summary(sec8c1)
+
+# sec8c2
+sec8c2 <- read_dta("02_raw_data/sec8c2.dta")
+View(sec8c2)
+summary(sec8c2)
+
+# sec8d
+sec8d <- read_dta("02_raw_data/sec8d.dta")
+View(sec8d)
+summary(sec8d)
+
+# sec8e
+sec8e <- read_dta("02_raw_data/sec8e.dta")
+View(sec8e)
+summary(sec8e)
+
+# sec8f
+sec8f <- read_dta("02_raw_data/sec8f.dta")
+View(sec8f)
+summary(sec8f)
+
+# sec8g
+sec8g <- read_dta("02_raw_data/sec8g.dta")
+View(sec8g)
+summary(sec8g)
+
+# sec8h
+sec8h <- read_dta("02_raw_data/sec8h.dta")
+View(sec8h)
+summary(sec8h)
+
+# sec8hid
+sec8hid <- read_dta("02_raw_data/sec8hid.dta")
+View(sec8hid)
+summary(sec8hid)
+
+# start joining files
+
+
